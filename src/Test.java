0b6b2fcfdb65d971dc348db4df5aa3be1c4cbe43
LAPTOP-BC2P92Q8\ASUS <khoimapu2k5@gmail.com>
2025-09-09 22:05:17 +0700

    9/9/2025

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,6 @@ public class Test {
         System.out.println("Hello World");
         System.out.println("!!!!!");
         System.out.println("I'm here!!!");
+        System.out.println("WTF");
     }
 }
